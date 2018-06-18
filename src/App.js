712779f5a848da0c1ecde4c53fff8e299e525140
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-18 16:41:10 +0300

    Added movie component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [
+	{
+		id: 1,
+		title: 'Star Wars',
+	},
+	{
+		id: 2,
+		title: 'Spider Man',
+	},
+	{
+		id: 3,
+		title: 'Interstellar',
+	},
+];
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+				</header>
+				{movies.map(movie => (
+						<Movie key={movie.id} movie={movie} />
+					))}
+			</div>
+		);
+	}
 }
 
 export default App;
