e1b792e3bbe4d015aa87d3aeddcd89a9f764a83b
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-18 22:59:03 +0300

    Added styles and overdrive

--- src/App.js
+++ src/App.js
@@ -1,55 +1,30 @@
-/* eslint react/no-did-mount-set-state: 0 */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
+  Link,
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './Movie';
+import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
 
-const API_KEY = 'cad25d09e7670f002bbe0190f2192baa';
-const LANG = 'ru-RU';
-
-class App extends Component {
-  state = {
-    movies: [],
-  }
-
-  async componentDidMount() {
-    try {
-      // cad25d09e7670f002bbe0190f2192baa
-      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=${LANG}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
-      const movies = await res.json();
-
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Route path="/test" component={test} />
-          {this.state.movies.map(movie => <Movie key={movie.id} movieItem={movie} />)}
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
-
-const test = () => (
-  <h1>YEKSJFG</h1>
-);
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
+import Overdrive from 'react-overdrive';
 
-const POSTER_PATH = 'https://image.tmdb.org/t/p/w500';
+const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
 
 const movie = ({ movieItem }) => (
-  <div>
-    <img src={`${POSTER_PATH}${movieItem.poster_path}`} alt={movieItem.title} />
-  </div>
+  <Link to={`/${movieItem.id}`}>
+    <Overdrive id={movieItem.id}>
+      <img className="poster" src={`${POSTER_PATH}${movieItem.poster_path}`} alt={movieItem.title} />
+    </Overdrive>
+  </Link>
 );
 
 movie.propTypes = {
